52ec6730e7ccdeb55648aecdd693f40b062f8d9c
Marzan Islam <marzanislam999@gmail.com>
2022-11-29 23:20:51 +0600

    signIn and signUp

--- blog_app/lib/screens/signup.dart
+++ blog_app/lib/screens/signup.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:blog_app/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,11 +13,166 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.pink,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 40, color: Colors.black),
+                  )),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Name',
+                  labelText: 'Name',
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.grey,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(13)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.orange),
+                      borderRadius: BorderRadius.circular(13)),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Phone',
+                  labelText: 'Phone',
+                  prefixIcon: Icon(
+                    Icons.call,
+                    color: Colors.grey,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(13)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Colors.orange,
+                      ),
+                      borderRadius: BorderRadius.circular(13)),
+                  errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Color.fromARGB(255, 66, 125, 145),
+                      ),
+                      borderRadius: BorderRadius.circular(13)),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Email Address',
+                  labelText: 'Email',
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.grey,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(13)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Colors.orange,
+                      ),
+                      borderRadius: BorderRadius.circular(13)),
+                  errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Color.fromARGB(255, 66, 125, 145),
+                      ),
+                      borderRadius: BorderRadius.circular(13)),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: _addressController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Address',
+                  labelText: 'Address',
+                  prefixIcon: Icon(
+                    Icons.home,
+                    color: Colors.grey,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(13)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Colors.orange,
+                      ),
+                      borderRadius: BorderRadius.circular(13)),
+                  errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Color.fromARGB(255, 66, 125, 145),
+                      ),
+                      borderRadius: BorderRadius.circular(13)),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => SignIn()));
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color.fromARGB(255, 125, 161, 126)),
+                  child: Center(
+                      child: Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  )),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
